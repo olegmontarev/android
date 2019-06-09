2836ba4dcb16bdf33f5fc26c20a58e2a9a28a24d
olegmont <olegmontarev@gmai.com>
2019-06-09 14:02:04 +0300

    Удалил файлы предыдущего домашнего задания

--- Cat.java
+++
@@ -1,7 +0,0 @@
-package Lesson_1.Marafon;
-
-public class Cat extends Animal {
-    public Cat(String name) {
-        super("Кот", name, 200, 20, 0);
-    }
-}
--- Dog.java
+++
@@ -1,7 +0,0 @@
-package Lesson_1.Marafon;
-
-public class Dog extends Animal {
-    public Dog(String name) {
-        super("Пес", name, 500, 5, 20);
-    }
-}
